0db868068e914e8904e6e00e14cc20c3cb838cd6
Manu <emmanuel@official.fm>
2012-08-28 18:17:04 +0200

    works

--- timelp/urls.py
+++ timelp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -15,3 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- timelp/views.py
+++ timelp/views.py
@@ -5,5 +5,8 @@ import discogs_client as discogs
 # Create your views here.
 def home(request):
 	discogs.user_agent = 'TimeLP/0.1'
-	collection = discogs.Collection('neutralino1').all(sort='year', sort_order='desc')
-	return render_to_response('home.html', {'collection':collection})
+	collection = discogs.User('neutralino1').collection(sort='year', order='desc')#, per_page=40)
+	years = []
+	for y in range(collection[0].year, collection[-1].year, -1):
+		years.append({'year': y, 'releases': [r for r in collection if r.year == y]})
+	return render_to_response('home.html', {'years':years})
